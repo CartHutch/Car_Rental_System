ae330a8c2087f15da5758daa98154256eb64aec8
Swara Nirav Dolia <swaradolia@gmail.com>
2026-07-31 22:52:55 -0400

    Remove trailing slash from BASE_URL

--- frontend/api.js
+++ frontend/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://car-rental-system-23td.onrender.com/';
+const BASE_URL = 'https://car-rental-system-23td.onrender.com';
 
 /* ===== Low Level Helpers =====
    - Internal wrapper around fetch.
